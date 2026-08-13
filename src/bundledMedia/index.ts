3d3d93a0b953c4c0277fa38025edbbcdd389d1cd
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-08-13 18:28:32 +0200

    Add bundled media subpath

---
+++ src/bundledMedia/index.ts
@@ -0,0 +1,12 @@
+export {
+  createExpoBundledMediaResolver,
+  type ExpoBundledMediaRegistry,
+  type ExpoBundledMediaResolver,
+  type ExpoBundledMediaResolverArgs,
+  type ExpoBundledMediaValue,
+  resolveExpoBundledMediaAsset,
+} from '../bundledMediaResolver';
+export {
+  type ExpoBundledMediaRegistrySourceEntry,
+  getExpoBundledMediaRegistrySource,
+} from '../bundledMediaRegistrySource';
